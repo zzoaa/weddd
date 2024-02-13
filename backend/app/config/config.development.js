13d4e38dd4151b008c5db30ede7d488f3538b2e5
Hyeonji-Son <sustka46@gmail.com>
2024-02-13 19:40:31 +0900

    [240213/손현지] BACK - config 주석

--- backend/app/config/config.development.js
+++ backend/app/config/config.development.js
@@ -1,19 +1,19 @@
 module.exports = {
     apiVersion: 'v1',
-    appPort: 7999,
+    appPort: 7999, /*local에서 번호 겹치지 않도록 설정*/
     secretKey: 'wheeparam',
     database: {
         host: '115.68.185.124',
         username: 'hagunbiz',
         password: 'gkrns!@#',
         port: 3306,
-        database: 'node_board'
+        database: 'node_board' /*local에서 사용할 DB명*/
     },
     cors: {
         origin: true,
         credentials: true
     },
-    apiUrl: 'http://localhost:7999',
+    apiUrl: 'http://localhost:7999', /*appPort와 번호 맞추기*/
     jwt: {
         accessTokenExpire: '15m',
         refreshTokenExpire: '14d',
--- backend/app/config/config.production.js
+++ backend/app/config/config.production.js
@@ -1,19 +1,19 @@
 module.exports = {
     apiVersion: 'v1',
-    appPort: 7999,
+    appPort: 7999, /*서버에서 번호 겹치지 않도록 설정*/
     secretKey: 'wheeparam',
     database: {
         host: '127.0.0.1',
         username: 'hagunbiz',
         password: 'gkrns!@#',
         port: 3306,
-        database: 'node_board'
+        database: 'node_board' /*local에서 사용할 DB명*/
     },
     cors: {
         origin: true,
         credentials: true
     },
-    apiUrl: 'http://localhost:7999',
+    apiUrl: 'http://localhost:7999', /*실제 사용할 서버 도메인 주소 입력*/
     jwt: {
         accessTokenExpire: '2h',
         refreshTokenExpire: '14d',
